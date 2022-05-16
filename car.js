bdada031e03012bb0ce54b429c302ba7d470bf46
Andrew Brown <andrewbrown986@gmail.com>
2022-05-16 11:46:28 -0400

    Added car movement/physics

--- car.js
+++ car.js
@@ -5,17 +5,72 @@ class Car{
         this.width=width;
         this.height=height;
 
-        this.controls = new Controls();
+        this.speed=0;
+        this.acceleration=0.2;
+        this.maxSpeed = 3;
+        this.friction = 0.05;
+        this.angle = 0;
 
+        this.controls = new Controls();
     }
-draw(ctx){
-    ctx.beginPath();
-    ctx.rect(
-        this.x-this.width/2,
-        this.y-this.height/2,
-        this.width,
-        this.height
-    );
-    ctx.fill();
-}
+
+    update(){
+        this.#move();
+    }
+
+    #move(){
+        if(this.controls.forward){
+            this.speed+=this.acceleration;  
+        }
+        if(this.controls.reverse){
+            this.speed-=this.acceleration;
+        }
+        if(this.speed>this.maxSpeed){ //cap the speed at the max speed 
+            this.speed = this.maxSpeed;
+        }
+        if(this.speed<-this.maxSpeed/2){
+            this.speed = -this.maxSpeed/2; //not as fast in reverse
+        }
+
+        if(this.speed>0){ //friction for forward motion
+            this.speed-=this.friction; //subtract the friction value from speed to slow down
+        }
+        if(this.speed<0){ //friction for reverse motion
+            this.speed+=this.friction;
+        }
+        if(Math.abs(this.speed)<this.friction){ //prevents drifting if speed is not exactly 0
+            this.speed=0;
+        }
+
+        if(this.speed!=0){
+            //flip is used to make driving in reverse work properly
+            const flip=this.speed>0?1:-1;//if speed >0 no flip else flip
+            if(this.controls.left){
+                this.angle+=0.03*flip;
+            }
+            if(this.controls.right){
+                this.angle-=0.03*flip;
+            }
+        }
+        
+        //update the x, y coordinates
+        this.x-=Math.sin(this.angle)*this.speed;
+        this.y-=Math.cos(this.angle)*this.speed;
+    }
+    draw(ctx){
+        ctx.save();
+        ctx.translate(this.x,this.y);
+        ctx.rotate(-this.angle);
+
+        ctx.beginPath();
+        ctx.rect(
+            -this.width/2,
+            -this.height/2,
+            this.width,
+            this.height
+        );
+        ctx.fill();
+
+        ctx.restore();
+    }
 }
--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 const canvas=document.getElementById("myCanvas");
-canvas.height=window.innerHeight;
+
 canvas.width=200;
 
 const ctx = canvas.getContext("2d");
 const car = new Car(100,100,30,50);
-car.draw(ctx);
+
+animate();
+
+function animate(){ //function allowing the movements of the car to take effect
+car.update(); //get new positions from button pushes 
+canvas.height=window.innerHeight; //resize canvas
+car.draw(ctx); //draw the car in the new position 
+requestAnimationFrame(animate); //calls the animate function repeatedly 
+}
