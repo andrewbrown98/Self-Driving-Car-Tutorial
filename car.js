b02bd18fcdb5fbe93fa50842b78fdd1bf104b617
Andrew Brown <andrewbrown986@gmail.com>
2022-05-20 09:44:54 -0400

    Added collision detection with road borders

--- car.js
+++ car.js
@@ -10,6 +10,7 @@ class Car{
         this.maxSpeed = 3;
         this.friction = 0.05;
         this.angle = 0;
+        this.damaged = false;
 
         //sensors
         this.sensor = new Sensor(this); //pass car to sensor
@@ -18,8 +19,46 @@ class Car{
     }
 
     update(roadBorders){
-        this.#move();
+        if(!this.damaged){ //If the car is damaged it stops moving
+            this.#move();
+            this.polygon = this.#createPolygon();
+            this.damaged= this.#assessDamage(roadBorders);
+        }
         this.sensor.update(roadBorders);
+    }
+
+    #assessDamage(roadBorders){
+        for(let i=0; i<roadBorders.length;i++){
+            if(polysIntersect(this.polygon,roadBorders[i])){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    #createPolygon(){
+        const points = [];
+        const rad = Math.hypot(this.width,this.height)/2;
+        const alpha = Math.atan2(this.width,this.height);
+        
+        //Get the corner points of the car and push to points
+        points.push({
+            x:this.x-Math.sin(this.angle-alpha)*rad,
+            y:this.y-Math.cos(this.angle-alpha)*rad
+        });
+        points.push({
+            x:this.x-Math.sin(this.angle+alpha)*rad,
+            y:this.y-Math.cos(this.angle+alpha)*rad
+        });
+        points.push({
+            x:this.x-Math.sin(Math.PI+this.angle-alpha)*rad,
+            y:this.y-Math.cos(Math.PI+this.angle-alpha)*rad
+        });
+        points.push({
+            x:this.x-Math.sin(Math.PI+this.angle+alpha)*rad,
+            y:this.y-Math.cos(Math.PI+this.angle+alpha)*rad
+        });
+        return points;
     }
 
     #move(){
@@ -62,20 +101,18 @@ class Car{
         this.y-=Math.cos(this.angle)*this.speed;
     }
     draw(ctx){
-        ctx.save();
-        ctx.translate(this.x,this.y);
-        ctx.rotate(-this.angle);
-
-        ctx.beginPath();
-        ctx.rect(
-            -this.width/2,
-            -this.height/2,
-            this.width,
-            this.height
-        );
-        ctx.fill();
-
-        ctx.restore();
+        if(this.damaged){
+            ctx.fillStyle="grey";
+        }
+        else{
+            ctx.fillStyle="black";
+        }
+       ctx.beginPath();
+       ctx.moveTo(this.polygon[0].x,this.polygon[0].y);
+       for(let i=1; i<this.polygon.length;i++){
+           ctx.lineTo(this.polygon[i].x,this.polygon[i].y)
+       }
+       ctx.fill();
 
         this.sensor.draw(ctx); //car draws its sensors
     }
