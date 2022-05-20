b56d3e4b9c4c64b1ec45a7802991326bc774eb2a
Andrew Brown <andrewbrown986@gmail.com>
2022-05-20 12:51:18 -0400

    Road traffic now added and sensors/car detect collisions with walls/traffic

--- main.js
+++ main.js
@@ -4,19 +4,32 @@ canvas.width=200;
 
 const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width/2,canvas.width*0.9);
-const car = new Car(road.getLaneCenter(1),100,30,50);
+const car = new Car(road.getLaneCenter(1),100,30,50,"KEYS");
+
+const traffic = [
+    new Car(road.getLaneCenter(1),-100,30,50,"DUMMY",2)
+];
 
 animate();
 
 function animate(){ //function allowing the movements of the car to take effect
-    car.update(road.borders); //get new positions from button pushes, pass the boarders for use with sensors
+    for(let i=0;i<traffic.length;i++){
+        traffic[i].update(road.borders,[])
+    }
+    
+    car.update(road.borders,traffic); //get new positions from button pushes, pass the boarders for use with sensors
     
     canvas.height=window.innerHeight; //resize canvas
     
     ctx.save(); // save the caanvas position
     ctx.translate(0,-car.y+canvas.height*0.7); // translate the canvas
     road.draw(ctx);
-    car.draw(ctx); //draw the car in the new position 
+
+    for(let i =0; i<traffic.length;i++){ //draw traffic
+        traffic[i].draw(ctx,"red");
+    }
+
+    car.draw(ctx, "blue"); //draw the car in the new position 
 
     ctx.restore();
     requestAnimationFrame(animate); //calls the animate function repeatedly 
